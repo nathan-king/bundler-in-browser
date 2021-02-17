263cd3113f7ba705f96906df383330ee2897e8ee
nathan-king <nath.king93@gmail.com>
2021-02-17 18:34:21 +1100

    Fix TS esbuild promise error

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -43,22 +43,22 @@ export const unpkgPathPlugin = () => {
           };
         }
 
-        // Check if file is already fetched and if in cache
-        const cachedResult = await fileCache.getItem(args.path);
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
 
-        // If in cache, return immediately
         if (cachedResult) {
           return cachedResult;
         }
 
         const { data, request } = await axios.get(args.path);
 
-        const result = {
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
-        // Store response in cache
+
         await fileCache.setItem(args.path, result);
 
         return result;
